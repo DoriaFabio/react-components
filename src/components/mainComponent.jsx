f677d899e197ab7d7dfff6e917fcf999aa6b5b60
DoriaFabio <doriafabio2001@gmail.com>
2024-12-12 18:57:31 +0100

    create card into main

--- src/components/mainComponent.jsx
+++ src/components/mainComponent.jsx
@@ -1,11 +1,18 @@
 function mainComponent() {
-    const title = "Task Manager";
-  
-    return (
-      <header className="mybg">
-        <h1 className="py-3 container-fluid">{title}</h1>
-      </header>
-    );
-  }
-  
-  export default mainComponent;
+  return (
+    <>
+      <main className="d-flex justify-content-center my-5">
+        <div className="card mycard">
+          <img src="" className="card-img-top" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
+            <a href="#" className="btn btn-warning">Go somewhere</a>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default mainComponent;
