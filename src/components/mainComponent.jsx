a0aee843385151b23ee219f6933b3ac866f077ba
DoriaFabio <doriafabio2001@gmail.com>
2024-12-12 19:53:17 +0100

    add background image at the hover of the card

--- src/components/mainComponent.jsx
+++ src/components/mainComponent.jsx
@@ -1,13 +1,13 @@
 function mainComponent() {
   return (
     <>
-      <main className="d-flex justify-content-center my-4">
+      <main className="d-flex justify-content-center my-5">
         <div className="card mycard">
           <img src="../../public/bulbasaur.png" className="card-img-top" alt="" />
           <div className="card-body">
             <h5 className="card-title">Bulbasaur</h5>
-            <p className="card-text">Bulbasaur è un Pokémon di doppio tipo Erba/Veleno introdotto in prima generazione.
-              Si evolve in Ivysaur a partire dal livello 16 e quindi in Venusaur a partire dal livello 32.
+            <p className="card-text">Bulbasaur è un Pokémon di doppio tipo Erba/Veleno.
+              Si evolve in Ivysaur e quindi in Venusaur.
               Insieme a Charmander e Squirtle, Bulbasaur è uno dei tre Pokémon iniziali di Kanto.</p>
             <a href="#" className="btn btn-warning">Leggi di più</a>
           </div>
